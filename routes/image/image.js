f7286e7242be23d73ec22cac8154968292e3ab33
Ben <ben.pearce@sky.com>
2021-02-16 13:12:47 +0000

    Initial routing packages

---
+++ routes/image/image.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const Buffer = require('buffer').Buffer;
+const Config = require('../../server.config');
+
+module.exports = (fastify, opts, done) => {
+
+  fastify.get('/image/:objectId/thumb', async function (req, rep) {
+    var thumbnail = await this.models.collectionsObjectImage.findOne({
+      where: {
+        object_id: req.params.objectId,
+        is_thumb: true
+      }
+    });
+
+    if(thumbnail === null) {
+      rep.code(404).send('Not found');
+    } else {
+      var imagePublicPath = `${Config.images.src}${thumbnail.imagePublicPath}`;
+      var raw = await axios.get(imagePublicPath, {
+        responseType: 'arraybuffer'
+      });
+  
+      rep
+        .code(200)
+        .header('Content-type', raw.headers['content-type'])
+        .send(Buffer.from(raw.data, 'binary'));
+    }
+  });
+
+  fastify.get('/image/:objectId/:imageId', async function (req, rep) {
+    var image = await this.models.collectionsObjectImage.findOne({
+      offset: req.params.imageId,
+      where: {
+        object_id: req.params.objectId,
+        is_thumb: false
+      }
+    });
+
+    if(image === null) {
+      rep.code(404).send('Not found');
+    } else {
+      var imagePublicPath = `${Config.images.src}${image.imagePublicPath}`;
+      console.log(imagePublicPath);
+      var raw = await axios.get(imagePublicPath, {
+        responseType: 'arraybuffer'
+      });
+
+      rep
+        .code(200)
+        .header('Content-type', raw.headers['content-type'])
+        .send(Buffer.from(raw.data, 'binary'));
+    }
+  });
+
+  done();
+};
---
+++ routes/search/search.js
@@ -0,0 +1,45 @@
+var JSONAPISerializer = require('jsonapi-serializer').Serializer;
+
+var ObjectSerializer = new JSONAPISerializer('object', {
+  attributes: [
+    'name', 
+    'creationEarliest',
+    'creationLatest',
+    'collectionsObjectImages',
+    'onDisplayAt'
+  ],
+  collectionsObjectImages: {
+    ref: (object, image) => 
+      `${object.id}/${object.collectionsObjectImages.indexOf(image)}`,
+    attributes: [
+      'imagePublicPath',
+      'isThumb'
+    ]
+  },
+  keyForAttribute: 'camelCase'
+});
+
+module.exports = (fastify, opts, done) => {
+
+  fastify.get('/search', async function (req, rep) {
+    var page = req.query.page ? req.query.page : 0;
+    var limit = req.query.limit ? req.query.limit : 10;
+    
+    var objects = await this.models.collectionsObject.findAll({
+      offset: page,
+      limit: limit,
+      include: {
+        model: this.models.collectionsObjectImage, 
+        as: 'collectionsObjectImages',
+        where: {
+          is_thumb: true
+        }
+      }
+    });
+
+    var serializedObjects = ObjectSerializer.serialize(objects);
+    rep.send(serializedObjects);
+  });
+
+  done();
+};
