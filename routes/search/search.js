6a187a39f19488f0fd09054a0bfbcf5071ba6201
Ben <ben.pearce@sky.com>
2021-02-21 16:05:08 +0000

    Basic support for query parameter on search route

--- routes/search/search.js
+++ routes/search/search.js
@@ -1,3 +1,4 @@
+var { Op } = require('sequelize');
 var JSONAPISerializer = require('jsonapi-serializer').Serializer;
 
 var ObjectSerializer = new JSONAPISerializer('object', {
@@ -25,8 +26,9 @@ module.exports = (fastify, opts, done) => {
   fastify.get('/search', async function (req, rep) {
     var page = req.query.page ? req.query.page : 0;
     var limit = req.query.limit ? req.query.limit : 10;
-    
-    var objects = await this.models.collectionsObject.findAll({
+    var query = req.query.q;
+
+    var queryOptions = {
       offset: page * limit,
       limit: limit,
       include: [{
@@ -38,9 +40,19 @@ module.exports = (fastify, opts, done) => {
       }, {
         model: this.models.collectionsObjectCategory,
         as: 'category'
-      }]
-    });
+      }],
+      where: {}
+    };
+    
+    if(query) {
+      Object.assign(queryOptions.where, {
+        [Op.or]: [{
+          name: {[Op.iLike]: `%${query}%`}
+        }]
+      });
+    }
 
+    var objects = await this.models.collectionsObject.findAll(queryOptions);
     var serializedObjects = ObjectSerializer.serialize(objects);
     rep.send(serializedObjects);
   });
