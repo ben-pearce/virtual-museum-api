be155fb90fd38303a352de79dca594d761cec165
Ben <ben.pearce@sky.com>
2021-02-28 22:26:13 +0000

    Add related objects meta data to person and object API endpoints

--- routes/object/object.js
+++ routes/object/object.js
@@ -51,7 +51,99 @@ module.exports = (fastify, opts, done) => {
         .code(404)
         .send('Not found');
     } else {
+      var relatedObjectsIncludes = [];
+
+      for(var maker of object.collectionsObjectMakers) {
+        relatedObjectsIncludes.push({
+          model: this.models.collectionsObjectMaker,
+          as: 'collectionsObjectMakers',
+          where: {
+            personId: maker.personId
+          }
+        });
+      }
+
+      for(var person of object.collectionsObjectPeople) {
+        relatedObjectsIncludes.push({
+          model: this.models.collectionsObjectPerson,
+          as: 'collectionsObjectPeople',
+          where: {
+            personId: person.personId
+          }
+        });
+      }
+
+      for(var place of object.collectionsObjectPlaces) {
+        relatedObjectsIncludes.push({
+          model: this.models.collectionsObjectPlace,
+          as: 'collectionsObjectPlaces',
+          where: {
+            placeId: place.placeId
+          }
+        });
+      }
+
+      var relatedObjectsQueries = [];
+      for(let include of relatedObjectsIncludes) {
+        relatedObjectsQueries.push(this.models.collectionsObject.findAll({
+          limit: 4,
+          include: [{
+            model: this.models.collectionsObjectImage, 
+            as: 'collectionsObjectImages',
+            where: {
+              isThumb: true
+            },
+            required: false
+          }, {
+            model: this.models.collectionsObjectCategory,
+            as: 'category'
+          }, include],
+          where: {
+            [Op.not]: {
+              id: object.id
+            }
+          }
+        }));
+      }
+
+      relatedObjectsQueries.push(this.models.collectionsObject.findAll({
+        limit: 4,
+        include: [{
+          model: this.models.collectionsObjectImage, 
+          as: 'collectionsObjectImages',
+          where: {
+            isThumb: true
+          },
+          required: false
+        }, {
+          model: this.models.collectionsObjectCategory,
+          as: 'category'
+        }],
+        where: {
+          categoryId: object.categoryId,
+          [Op.not]: {
+            id: object.id
+          }
+        }
+      }));
+
+      var queryResults = await Promise.all(relatedObjectsQueries);
+
+      var relatedObjects = queryResults.flat().filter((object, index, self) => 
+        index === self.findIndex((o) => (
+          o.id === object.id
+        ))
+      ).slice(0, 4);
+
       var serializedObject = ObjectSerializer.serialize(object);
+
+      serializedObject = {
+        ...serializedObject,
+        meta: {
+          relatedObjects: ObjectResultSerializer.serialize(relatedObjects)
+        }
+      };
+
       rep.send(serializedObject);
     }
   });
--- routes/person/person.js
+++ routes/person/person.js
@@ -4,18 +4,67 @@ var ObjectResultSerializer = require('../../serializers/objectResultSerializer')
 module.exports = (fastify, opts, done) => {
 
   fastify.get('/person/:personId', async function (req, rep) {
-    var object = await this.models.collectionsPerson.findOne({
+    var person = await this.models.collectionsPerson.findOne({
       where: {
         id: req.params.personId
       }
     });
 
-    if(object === null) {
+    if(person === null) {
       rep
         .code(404)
         .send('Not found');
     } else {
-      var serializedObject = PersonSerializer.serialize(object);
+      var relatedObjectsIncludes = [{
+        model: this.models.collectionsObjectMaker,
+        as: 'collectionsObjectMakers',
+        where: {
+          personId: person.id
+        }
+      }, {
+        model: this.models.collectionsObjectPerson,
+        as: 'collectionsObjectPeople',
+        where: {
+          personId: person.id
+        }
+      }];
+  
+      var relatedObjectsQueries = [];
+  
+      for(var include of relatedObjectsIncludes) {
+        relatedObjectsQueries.push(this.models.collectionsObject.findAll({
+          limit: 4,
+          include: [{
+            model: this.models.collectionsObjectImage, 
+            as: 'collectionsObjectImages',
+            where: {
+              isThumb: true
+            },
+            required: false
+          }, {
+            model: this.models.collectionsObjectCategory,
+            as: 'category'
+          }, include]
+        }));
+      }
+      
+      var queryResults = await Promise.all(relatedObjectsQueries);
+  
+      var relatedObjects = queryResults.flat().filter((object, index, self) => 
+        index === self.findIndex((o) => (
+          o.id === object.id
+        ))
+      ).slice(0, 4);
+
+      var serializedObject = PersonSerializer.serialize(person);
+
+      serializedObject = {
+        ...serializedObject,
+        meta: {
+          relatedObjects: ObjectResultSerializer.serialize(relatedObjects)
+        }
+      };
+
       rep.send(serializedObject);
     }
   });
