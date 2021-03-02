e0a5f0350dc47ea88bac510f2fddbc5058720663
Ben <ben.pearce@sky.com>
2021-03-02 11:48:38 +0000

    User object favourites sequelize model

---
+++ models/userCollectionsObjectFavourite.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  return UserCollectionsObjectFavourite.init(sequelize, DataTypes);
+};
+
+class UserCollectionsObjectFavourite extends Sequelize.Model {
+  static init(sequelize, DataTypes) {
+    super.init({
+      userId: {
+        field: 'user_id',
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        comment: 'Liker user ID',
+        primaryKey: true,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      },
+      objectId: {
+        field: 'object_id',
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        comment: 'Liked object ID',
+        primaryKey: true,
+        references: {
+          model: 'collections_object',
+          key: 'id'
+        }
+      }
+    }, {
+      sequelize,
+      tableName: 'user_collections_object_favourite',
+      schema: 'public',
+      timestamps: false,
+      indexes: [
+        {
+          name: 'user_collections_object_favourite_pkey',
+          unique: true,
+          fields: [
+            { name: 'user_id' },
+            { name: 'object_id' },
+          ]
+        },
+      ]
+    });
+    return UserCollectionsObjectFavourite;
+  }
+}
